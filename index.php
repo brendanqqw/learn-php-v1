28ebc75d61d3c6f18a0b40fef40879b93b5d6062
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-27 20:42:43 +0800

    v1.0

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
-echo "Hello World!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learn PHP</title>
+</head>
+<body>
+    <?php
+        echo "Hello Brendan!";
+    ?>
+</body>
+</html>
